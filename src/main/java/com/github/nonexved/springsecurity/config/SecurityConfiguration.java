8e81280c13e915039dfdec739d598fe33c0c5489
Damian Bajno <bajno.damian96@gmail.com>
2021-10-21 17:20:13 +0200

    Added users whos are authenticted in memory

---
+++ src/main/java/com/github/nonexved/springsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.github.nonexved.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user1")
+            .password("{bcrypt}$2a$06$Hwx3rOkXLPnTivEIO3kqSe9j1jnjFeNKzv4Zthi5OHBFR9KtVFqwe")
+            .roles("USER")
+            .and()
+            .withUser("user2")
+            .password("{bcrypt}$2a$06$Hwx3rOkXLPnTivEIO3kqSe9j1jnjFeNKzv4Zthi5OHBFR9KtVFqwe")
+            .roles("USER");
+    }
+}
