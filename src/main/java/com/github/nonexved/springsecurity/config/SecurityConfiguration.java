4a9c12f71973c753070a94bcc7a77d32297643c5
Damian Bajno <bajno.damian96@gmail.com>
2021-10-21 17:42:43 +0200

    Added authorization for role USER by http basic

--- src/main/java/com/github/nonexved/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/github/nonexved/springsecurity/config/SecurityConfiguration.java
@@ -2,10 +2,22 @@ package com.github.nonexved.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/secured/**", "/api/secured/**")
+            .hasRole("USER")
+            .and()
+            .formLogin()
+            .and()
+            .httpBasic();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -16,6 +28,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .withUser("user2")
             .password("{bcrypt}$2a$06$Hwx3rOkXLPnTivEIO3kqSe9j1jnjFeNKzv4Zthi5OHBFR9KtVFqwe")
-            .roles("USER");
+            .roles("TEST");
     }
 }
