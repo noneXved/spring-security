7f11e9c5c8aa123b240225fe5f447741a430b5cd
Damian Bajno <bajno.damian96@gmail.com>
2021-10-23 23:43:28 +0200

    Added Keyclock authentication

--- src/main/java/com/github/nonexved/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/github/nonexved/springsecurity/config/SecurityConfiguration.java
@@ -1,37 +1,56 @@
 package com.github.nonexved.springsecurity.config;
 
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
+@KeycloakConfiguration
 @Order(3)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http.authorizeRequests()
             .antMatchers("/secured/**", "/api/secured/**")
             .hasRole("USER")
             .and()
-            .formLogin()
-            .and()
-            .httpBasic()
-            .and()
             .oauth2Login();
+    }
+
+    @Bean
+    @Override
+    @ConditionalOnMissingBean(HttpSessionManager.class)
+    protected HttpSessionManager httpSessionManager() {
+        return new HttpSessionManager();
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("user1")
-            .password("{bcrypt}$2a$06$Hwx3rOkXLPnTivEIO3kqSe9j1jnjFeNKzv4Zthi5OHBFR9KtVFqwe")
-            .roles("USER")
-            .and()
-            .withUser("user2")
-            .password("{bcrypt}$2a$06$Hwx3rOkXLPnTivEIO3kqSe9j1jnjFeNKzv4Zthi5OHBFR9KtVFqwe")
-            .roles("TEST");
+        SimpleAuthorityMapper mapper = new SimpleAuthorityMapper();
+        mapper.setPrefix("ROLE_");
+
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(mapper);
+
+        auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 }
